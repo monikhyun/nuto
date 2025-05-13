ef606495d0575ada21afcbd7ed5112c118f37a58
monikhyun <mih2001103@naver.com>
2025-05-13 19:33:40 +0900

    feat / 최초 커밋

---
+++ src/main/java/goorm/nuto/Nuto/NutoApplication.java
@@ -0,0 +1,13 @@
+package goorm.nuto.Nuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goorm/nuto/Nuto/NutoApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.nuto.Nuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
