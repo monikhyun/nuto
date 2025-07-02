e811a2ece350ea73371b0074d301d28dde990a43
monikhyun <mih2001103@naver.com>
2025-07-02 17:24:37 +0900

    feat / Exception 추가

--- src/main/java/goorm/nuto/Nuto/Exception/NotVerifiedEmailException.java
+++ src/main/java/goorm/nuto/Nuto/Exception/NotVerifiedEmailException.java
@@ -1,7 +1,7 @@
 package goorm.nuto.Nuto.Exception;
 
-public class NotVerifiedEmailException extends RuntimeException {
+public class NotVerifiedEmailException extends BusinessException {
   public NotVerifiedEmailException(String message) {
-    super(message);
+    super(ErrorCode.NOT_VERIFIED_EMAIL, message);
   }
 }
