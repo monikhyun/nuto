f87a6825cf6facee5ebccba6737c9bd80d561721
monikhyun <mih2001103@naver.com>
2025-07-05 20:50:48 +0900

    feat / 소비 및 수익 Repository 메서드 추가

--- src/main/java/goorm/nuto/Nuto/Repository/IncomeRepository.java
+++ src/main/java/goorm/nuto/Nuto/Repository/IncomeRepository.java
@@ -1,7 +1,17 @@
 package goorm.nuto.Nuto.Repository;
 
 import goorm.nuto.Nuto.Entity.Income;
+import goorm.nuto.Nuto.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+    @Query("SELECT COALESCE(SUM(i.amount), 0) FROM Income i " +
+            "WHERE i.member = :member AND i.date BETWEEN :startDate AND :endDate")
+    Double getTotalIncomeBetweenDates(@Param("member") Member member,
+                                      @Param("startDate") LocalDate startDate,
+                                      @Param("endDate") LocalDate endDate);
 }
