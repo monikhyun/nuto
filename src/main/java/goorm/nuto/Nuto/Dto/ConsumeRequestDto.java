5acac7c4113c45a83d102d6cbf347dfabf2d6408
Hyeseon Lee <hyeseon1379@gmail.com>
2025-07-05 12:57:13 +0900

    feat / Consume 기능 구현

---
+++ src/main/java/goorm/nuto/Nuto/Dto/ConsumeRequestDto.java
@@ -0,0 +1,14 @@
+package goorm.nuto.Nuto.Dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class ConsumeRequestDto {
+    private String name;
+    private Long amount;
+    private LocalDate date;
+    private Long cardId;
+    private Long categoryId;
+}
