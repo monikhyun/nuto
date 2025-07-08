c08d9ade615c931f3551386dd9aed8ef2c8e9274
monikhyun <mih2001103@naver.com>
2025-07-08 19:27:01 +0900

    feat / 소비 기록 서비스 로직 구현

--- src/main/java/goorm/nuto/Nuto/Repository/ReceiptRepository.java
+++ src/main/java/goorm/nuto/Nuto/Repository/ReceiptRepository.java
@@ -1,7 +1,16 @@
 package goorm.nuto.Nuto.Repository;
 
+import goorm.nuto.Nuto.Entity.CategoryType;
+import goorm.nuto.Nuto.Entity.Member;
 import goorm.nuto.Nuto.Entity.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+    List<Receipt> findByMemberAndCategory_Type(Member member, CategoryType categoryType);
+    Optional<Receipt> findByMemberAndId(Member member, Long id);
+    List<Receipt> findByMemberAndCategory_TypeAndDateBetween(Member member, CategoryType type, LocalDate start, LocalDate end);
 }
